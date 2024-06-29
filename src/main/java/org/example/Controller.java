da23811d8cdd50b49a309b21eb5cad7caa45afc2
aadirai <aadi.rai2008@gmail.com>
2024-06-29 15:01:21 +1000

    Implemented Controller

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,100 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Controller {
+    private final ArrayList<User> users;
+    private final ArrayList<Booking> bookings;
+    private final ArrayList<Event> events;
+
+    private User currentUser;
+
+
+    public Controller() {
+        events = new ArrayList<Event>();
+        bookings =  new ArrayList<Booking>();
+        users =  new ArrayList<User>();
+    }
+
+    public boolean validateUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean login(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                currentUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //<editor-fold desc="getters/setters/add/remove/find methods">
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    public void removeEvent(Event event) {
+        events.remove(event);
+    }
+
+    public Event findEvent(String eventID) {
+        for (Event event : events) {
+            if (event.getEventID().equals(eventID)) {
+                return event;
+            }
+        }
+        return null;
+    }
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
+
+    public void removeBooking(Booking booking) {
+        bookings.remove(booking);
+    }
+
+    public Booking findBooking(String bookingID) {
+        for (Booking booking : bookings) {
+            if (booking.getBookingID().equals(bookingID)) {
+                return booking;
+            }
+        }
+        return null;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    public User findUser(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    //</editor-fold>
+}
